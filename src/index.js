7db3fcce292ad90041d5821a4b66a0b0d400d046
Thanh Trung <thanhtrung20022001@gmail.com>
2021-03-08 23:59:56 +0700

    Init project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // constructor(props){
+    //     super(props);
+
+    //     this.state({ lat: null, errorMessage: ''})
+
+        // window.navigator.geolocation.getCurrentPosition(
+        //     position => this.setState({lat: position.coords.latitude}), 
+        //     err => this.setState({errorMessage: err.message})
+        // );
+    // }
+
+
+    //Tự động viết thành contructor function
+    state = {
+        lat: null, 
+        errorMessage: ''
+    };
+
+    /**
+     * Được gọi khi đã hiển thị component ra ngoài trình duyệt, hàm này chỉ gọi 1 lần
+     * Hàm này được gọi để thông báo component đã tồn tại trên DOM, từ đó các thao tác trên DOM có thể thực hiện bình thường với component này.
+     */
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}), 
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    render () {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return (
+                <SeasonDisplay lat={this.state.lat} />
+            );
+        }
+
+        return <div>Loading...</div>
+    }
+};
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
